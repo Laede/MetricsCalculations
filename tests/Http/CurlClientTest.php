7a3e822552b5dc8c86851ff10bc05ba890f0a0d1
Roman <roman.mozolevskij@skycop.com>
2019-07-28 12:22:12 +0300

    Added a test to check 500 status code.

--- tests/Http/CurlClientTest.php
+++ tests/Http/CurlClientTest.php
@@ -5,29 +5,24 @@ namespace Supermetrics\Tests\Http;
 use PHPUnit\Framework\TestCase;
 use Supermetrics\Http\CurlClient;
 use Supermetrics\Http\RequestException;
+use Supermetrics\SupermetricsApi\ApiClient;
+use Supermetrics\SupermetricsApi\User;
 
 /**
  * Class CurlClientTest.
  */
 class CurlClientTest extends TestCase
 {
-    public function testReturnCorrectStatusCode()
+    public function testCheckIfBadIdReturns500ErrorOnTokenRequest(): void
     {
-        $client = new CurlClient('http://this-is-a-test.test');
-        try {
-            $client->get('testing');
-        } catch (RequestException $e) {
-
-        }
-    }
-
-    /**
-     * @expectedException \Supermetrics\Http\RequestException
-     */
-    public function testThrowsExceptionOnFail(): void
-    {
+        $statusCode = 500;
         $client = new CurlClient('https://api.supermetrics.com/assignment');
+        $user = new User('testIdm81mhid5ue1z3v2g0uh', 'Test Test', 'test@test.test');
 
-        $client->get('/test');
+        try {
+            $client->post('/test', $user->toArray());
+        } catch (RequestException $e) {
+           $this->assertSame($statusCode, $e->getCode());
+        }
     }
 }
